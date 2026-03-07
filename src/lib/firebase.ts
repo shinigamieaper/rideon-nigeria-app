a2b00faf1b416052cec9819b55ec568997d4a41b
oyekolaobajuwon <oyekolaobajuwon@gmail.com>
2026-03-07 23:25:55 +0100

    Use rideonnigeria.com as production Firebase auth domain

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -18,7 +18,7 @@ const firebaseConfig = {
   authDomain:
     process.env.NODE_ENV === "development"
       ? (process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN as string)
-      : "rideon-nigeria-app-roan.vercel.app",
+      : "rideonnigeria.com",
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID as string,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID as string,
   messagingSenderId: process.env
