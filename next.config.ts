9d65fa12a9823593bf499d0688665e3bfa7b9d89
oyekolaobajuwon <oyekolaobajuwon@gmail.com>
2026-03-08 00:46:37 +0100

    Add Vercel rewrite to proxy Firebase auth helpers to firebaseapp.com

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/__/auth/:path*",
+        destination: "https://rideon-e06da.firebaseapp.com/__/auth/:path*",
+      },
+    ];
+  },
   images: {
     remotePatterns: [
       {
